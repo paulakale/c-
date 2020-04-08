131f3044a7b3713380654286c99d084824b13897
Paula Kale <paula.kale@gmail.com>
2020-04-08 18:18:27 -0300

    Adiciona exercicio

---
+++ exercicio/exercicio4/exercicio4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <locale>
+using namespace std;
+int main()
+//Receba a quantidade de dias que uma pessoa já viveu e informe quantos anos ela tem.
+//Desconsidere os anos bissextos. Informe se a pessoa já é maior de idade.
+{
+    setlocale (LC_ALL, "pt-br");
+    int days, years;
+    cout << "Quantos dias você já viveu? \n";
+    cin >> days;
+    years = days/365;
+    if (days/365 >=18)    
+    cout << "Você é maior de idade. \n";
+    if (days/365 < 18)
+    cout << "Você não é maior de idade! \n";
+}
